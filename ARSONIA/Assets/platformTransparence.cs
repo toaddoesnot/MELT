fad8ba111c0f2cca1fe51809430aa8300070ccf0
toaddoesnot <laiscusn7@gmail.com>
2020-03-13 04:19:47 +0300

    addpr

---
+++ ARSONIA/Assets/platformTransparence.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformTransparence : MonoBehaviour
+{
+    public float waiting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            StartCoroutine(FallTimer());
+        }
+    }
+
+    IEnumerator FallTimer()
+    {
+        GetComponent<EdgeCollider2D>().enabled = false;
+        GetComponent<Rigidbody2D>().gravityScale += 4;
+        yield return new WaitForSeconds(waiting);
+        GetComponent<EdgeCollider2D>().enabled = true;
+        GetComponent<Rigidbody2D>().gravityScale -= 4;
+    }
+}
